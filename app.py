ff641a9d1798d52131ec7ce75b04bc2b3b8a2501
Enterkey <enterkey88@gmail.com>
2015-01-14 03:24:44 +0900

    beat에 요청하는 trackID 값 형식 변경

--- app.py
+++ app.py
@@ -15,10 +15,16 @@ pictureList = []
 # akmu = "http://beatpacking.com/tracks/3000000000000000000000009ecd9c"
 # errorUrl = "http://beatpacking.com/tracks/3000000000000000000000009ecd97"
 
+trackIdprefix = "000000"
+
 # 16의 6승
 limit = pow(16, 6)
 for i in range(limit):
-    trackId = format(i, 'x')
+    trackIdToHex = format(i, 'x')
+    trackIdToHexLength = len(trackIdToHex)
+    tempTrackId = trackIdprefix + trackIdToHex
+    trackId = tempTrackId[trackIdToHexLength:]
+
     base_url = "http://beatpacking.com/tracks/300000000000000000000000" + trackId
 
     # 해당 URI가 404가 아닐때 가수명, 곡명, 사진을 가져옴
