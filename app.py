8fb8fe326fa85a0432cb7fa5e2a7420d98d7b978
Enterkey <enterkey88@gmail.com>
2015-01-14 10:31:23 +0900

    서버에서 크롤링 정보 받아오면 파일에 바로 쓰도록 기능 수정

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ import csv
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-artistList = []
-titleList = []
-trackIdList = []
-pictureList = []
-
 # akmu = "http://beatpacking.com/tracks/3000000000000000000000009ecd9c"
 # errorUrl = "http://beatpacking.com/tracks/3000000000000000000000009ecd97"
 
 trackIdprefix = "000000"
+
+# csv 파일명
+file_name = "beat_tracks_info"
+csvFile = open(file_name, 'wb')
+writer = csv.writer(csvFile, csv.excel)
 
 # 16의 6승
 limit = pow(16, 6)
@@ -30,32 +30,16 @@ for i in range(limit):
     # 해당 URI가 404가 아닐때 가수명, 곡명, 사진을 가져옴
     try:
         soup = BeautifulSoup(urlopen(base_url).read())
-        artists = soup.select('.container > .thumbnail > .caption > .track-data > .artist')
-        for artist in artists:
-            artistList.append(artist.get_text().strip())
+        artist = soup.select('.container > .thumbnail > .caption > .track-data > .artist')
+        title = soup.select('.container > .thumbnail > .caption > .track-data > .track-name')
+        picture = soup.select('.container > .thumbnail > .link > img')
+        writer.writerow([artist[0].get_text().strip(), title[0].get_text().strip(), picture[0]['src'].strip(), trackId])
 
-        titles = soup.select('.container > .thumbnail > .caption > .track-data > .track-name')
-        for title in titles:
-            titleList.append(title.get_text().strip())
-
-        pictures = soup.select('.container > .thumbnail > .link > img')
-        for picture in pictures:
-            pictureList.append(picture['src'].strip())
-
-        trackIdList.append(trackId)
-        print "success"
+        print "TrackId : %s Success" % trackId
 
     except IOError as e:
         print e
         continue
 
-# 저장한 가수명의 수만큼 반복문을 돌리기 위한 변수
-searchedItemlen = len(artistList)
 
-# csv 파일명
-file_name = "beat_tracks_info"
 
-with open(file_name, 'wb') as f:
-    writer = csv.writer(f, csv.excel)
-    for k in range(searchedItemlen):
-        writer.writerow([artistList[k], titleList[k], pictureList[k], trackIdList[k]])
