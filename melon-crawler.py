3b922c4600cb82bab163a64276ff60e0600504c4
SungpilNam <iimplicit@gmail.com>
2015-01-17 15:51:23 +0900

    modified melon crawler

--- melon-crawler.py
+++ melon-crawler.py
@@ -7,7 +7,6 @@ import time
 import re
 
 def scrapMelonMusicId(original_query):
-	original_query = "아이유 곰돌이"
 	# encode original_query into modified_query
 	modified_query = quote(original_query)
 	modified_url = "http://www.melon.com/search/song/index.htm?q=" + modified_query + "&section=&searchGnbYn=Y&ipath=srch_form"
@@ -19,12 +18,13 @@ def scrapMelonMusicId(original_query):
 	html = f.read().decode('utf-8')
 	soup = BeautifulSoup(html)
 	html_element = soup.select("#frm_defaultList > div > table > tbody > tr:nth-of-type(1) > td > div > input")
-	music_id_list = re.findall('value="(.*)"\s?', str(html_element))
+	music_id_list = re.findall(r'value="(.*)"\s?', str(html_element))
 	if(len(music_id_list) == 0):
 		print("=== not in the html content ===")
 	else:
 		music_id = music_id_list[0]
 		print(music_id)		
 	
-scrapMelonMusicId();
+scrapMelonMusicId("빅뱅 붉은 노을");
+
 
