6e4e9a17daca9c1e569bb0148424ae30f2a33894
SungpilNam <iimplicit@gmail.com>
2015-01-17 17:49:31 +0900

    멜론 스크래퍼 테스팅, 멜론 스크래퍼에서 안가져오던 menuId도 가져오게 기능 추가

--- melon-crawler.py
+++ melon-crawler.py
@@ -6,6 +6,17 @@ import csv
 import time
 import re
 
+##### Protocol #####
+### melon-ios: meloniphone://play?ctype=1&menuid=29020101&cid=5536206
+# - prefix: meloniphone://play?ctype=1
+# - appendix: &menuid=29020101&cid=5536206
+### melon-itunes: https://itunes.apple.com/kr/app/mellon-melon/id415597317?mt=8
+### melon-android: melonapp://play?ctype=1&menuid=29020101&cid=5536206
+# - prefix: melonapp://play?ctype=1
+# - appendix: &menuid=29020101&cid=5536206
+### melon-googleplay: market://details?id=com.iloen.melon
+##### End of Protocol #####
+
 def scrapMelonMusicId(original_query):
 	# encode original_query into modified_query
 	modified_query = quote(original_query)
@@ -17,15 +28,16 @@ def scrapMelonMusicId(original_query):
 	f = urlopen(modified_url)
 	html = f.read().decode('utf-8')
 	soup = BeautifulSoup(html)
-	html_element = soup.select("#frm_defaultList > div > table > tbody > tr:nth-of-type(1) > td > div > input")
-	music_id_list = re.findall(r'value="(.*)"\s?', str(html_element))
-	if(len(music_id_list) == 0):
-		print("music id not in the html content")
-	else:
-		print("found music id")
-		music_id = music_id_list[0]
-		return music_id		
+	html_element = soup.select("#frm_defaultList > div > table > tbody > tr:nth-of-type(1) > td.t_left > div > div > button:nth-of-type(1)")
+	if(len(html_element) == 0):
+		print("music id is not in the html content")
+	else: 
+		music_id_list = re.findall(r"playSong\((.*)\)", str(html_element))[0].split(",")
+		erase_quote = re.sub("'", "", music_id_list[0])
+		music_id_list[0] = erase_quote
+		final_music_id = "&menuid=" + music_id_list[0] + "&cid=" + music_id_list[1]
+		print("this is music id", final_music_id)
+		return final_music_id	
 	
-# scrapMelonMusicId("빅뱅 붉은 노을");
-
+# scrapMelonMusicId("빅뱅 붉은노을");
 
